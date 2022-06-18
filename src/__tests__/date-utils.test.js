6646209890f290d156502a913dff40ce3a9df1f5
Acrylic125 <acrylic125email@gmail.com>
2022-06-18 21:01:32 +0800

    test: added test for DateUtils formatAsShortDate

--- src/__tests__/date-utils.test.js
+++ src/__tests__/date-utils.test.js
@@ -1,5 +1,9 @@
-describe("DateUtils", () => {
-    it("should return the correct date", () => {
-        
+import { formatAsShortDate } from "../utils/date-utils";
+
+describe("Given the date object, 3/3/2022 at 12:00", () => {
+  describe("When I call formatAsShortDate", () => {
+    it("should format to '3 Mar'", () => {
+      expect(formatAsShortDate(new Date(2022, 2, 3))).toBe("3 Mar");
     });
+  });
 });
