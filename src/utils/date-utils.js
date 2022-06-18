ff40accf1fc4c65d1c6e64e5b93bacf59b7f15e5
Acrylic125 <acrylic125email@gmail.com>
2022-06-18 20:40:15 +0800

    feat: added date-utils.js

---
+++ src/utils/date-utils.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {Date} day
+ */
+export function getWeekDaysOf(day) {
+  return {};
+}
+
+/**
+ *
+ * @param {Date} date
+ */
+export function formatAsShortDate(date) {
+  return "";
+}
