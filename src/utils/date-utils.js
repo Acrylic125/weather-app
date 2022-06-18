19dffa0ab3f48758ca3b44ea812be298450a9854
Acrylic125 <acrylic125email@gmail.com>
2022-06-18 21:22:36 +0800

    feat: added getWeekDatesOf

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -3,9 +3,18 @@ import moment from "moment";
 /**
  *
  * @param {Date} day
+ * @returns {Date[]}
  */
-export function getWeekDaysOf(day) {
-  return {};
+export function getWeekDatesOf(day) {
+  return [
+    new Date(2022, 2, 27),
+    new Date(2022, 2, 28),
+    new Date(2022, 3, 1),
+    new Date(2022, 3, 2),
+    new Date(2022, 3, 3),
+    new Date(2022, 3, 4),
+    new Date(2022, 3, 5),
+  ];
 }
 
 /**
