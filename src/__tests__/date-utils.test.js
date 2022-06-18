b8d3575ab060dc7aaa2a2ada25c84aed21387512
Acrylic125 <acrylic125email@gmail.com>
2022-06-18 21:22:28 +0800

    test: getWeekDatesOf

--- src/__tests__/date-utils.test.js
+++ src/__tests__/date-utils.test.js
@@ -1,9 +1,23 @@
-import { formatAsShortDate } from "../utils/date-utils";
+import { formatAsShortDate, getWeekDatesOf } from "../utils/date-utils";
 
 describe("Given the date object, 3/3/2022 at 12:00", () => {
   describe("When I call formatAsShortDate", () => {
     it("should format to '3 Mar'", () => {
       expect(formatAsShortDate(new Date(2022, 2, 3))).toBe("3 Mar");
+    });
+  });
+
+  describe("When I call getWeekDaysOf", () => {
+    it("should get back the dates of the weeks for 3/3/2022", () => {
+      expect(getWeekDatesOf(new Date(2022, 2, 3))).toBe([
+        new Date(2022, 2, 27),
+        new Date(2022, 2, 28),
+        new Date(2022, 3, 1),
+        new Date(2022, 3, 2),
+        new Date(2022, 3, 3),
+        new Date(2022, 3, 4),
+        new Date(2022, 3, 5),
+      ]);
     });
   });
 });
